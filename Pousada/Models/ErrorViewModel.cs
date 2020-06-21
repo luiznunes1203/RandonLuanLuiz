12e85fe6d4784841c97d9866ee11766c6bf6fe3b
Luiz Nunes <nunes.luiz1203@gmail.com>
2020-06-21 02:10:02 -0300

    Criando estrutura do projeto

---
+++ Pousada/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pousada.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
